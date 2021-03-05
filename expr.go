2eea6c14db02c510ea47e977e57193dc26fe2ec3
Nicky Logan <laurentiusdominick@yahoo.com>
2021-03-05 23:45:34 +0800

    feat: add eval impl stub

--- expr.go
+++ expr.go
@@ -10,7 +10,8 @@ type Expression interface {
 var _ Expression = (*expression)(nil)
 
 type expression struct {
-	expr string
+	expr   string
+	tokens []Token
 }
 
 // NewExpression creates a new expression based on the given expression string.
@@ -20,7 +21,53 @@ func NewExpression(expr string) Expression {
 
 // Evaluate implements the Expression interface.
 func (e *expression) Evaluate() (res float64, err error) {
-	// TODO: implement method
+	t := NewTokenizer()
+
+	tokens, err := t.Tokenize(e.expr)
+	if err != nil {
+		return 0, err
+	}
+
+	rpn := e.toRPN(tokens)
+	return e.eval(rpn)
+}
+
+type tokenStack struct {
+	ts []Token
+}
+
+func (s tokenStack) top() Token {
+	if len(s.ts) == 0 {
+		return nil
+	}
+
+	return s.ts[len(s.ts)-1]
+}
+
+func (s tokenStack) pop() Token {
+	if len(s.ts) == 0 {
+		return nil
+	}
+
+	t := s.top()
+	s.ts = s.ts[:len(s.ts)-1]
+
+	return t
+}
+
+func (s tokenStack) push(t Token) {
+	s.ts = append(s.ts, t)
+}
+
+func (s tokenStack) len() int {
+	return len(s.ts)
+}
+
+func (e *expression) toRPN(tokens []Token) (rpn []Token) {
+	return
+}
+
+func (e *expression) eval(rpn []Token) (res float64, err error) {
 	return
 }
 
